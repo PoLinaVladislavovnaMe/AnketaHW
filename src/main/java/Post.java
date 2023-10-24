3cffc454666013908e71226ade85f2643df70c9d
Polina Moiseenko <trylyalya@rambler.ru>
2023-10-24 08:49:26 +0700

    Post, Main,FormDate added

---
+++ src/main/java/Post.java
@@ -0,0 +1,12 @@
+public class Post {
+    public String name; // поле для имени
+    public String surname; // поле для фамилии
+
+    public String passport; //поле для паспорта
+
+    public String patronymic; // поле для отчества
+    public String phone; // поле для телефона
+    public boolean subscription; // поле для согласия-отказа
+    public FormDate birthday; // поле для даты рождения
+
+}
